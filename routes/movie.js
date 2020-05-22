ebde7f833e46addf7a425c3498969da347f8eff1
anilpdv <pdvanil007@gmail.com>
2020-05-23 00:36:19 +0530

    testing

--- routes/movie.js
+++ routes/movie.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const axios = require("axios");
 const publicIp = require("public-ip");
 const { key, secret_key } = require("./.config.js");
-const getIP = require("external-ip")();
+const requestIp = require("request-ip");
 
 router.get("/:id", async (req, res) => {
   try {
@@ -14,9 +14,11 @@ router.get("/:id", async (req, res) => {
     let ips = req.ips;
     let reqip = req.ip;
     let sip = req.socket.address().address;
+    const clientIp = requestIp.getClientIp(req);
+
     console.log(req.ips);
     const url = await get_movie(req.params.id, ip);
-    res.json({ url: url, ips, reqip, ip, sip });
+    res.json({ url: url, ips, reqip, ip, sip, clientIp });
   } catch (err) {
     console.log(err);
     res.status(400);
