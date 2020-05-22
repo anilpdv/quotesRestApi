1f11ee2f6d9c798c668679a4edf5b3ee91e6a331
anilpdv <pdvanil007@gmail.com>
2020-05-22 23:26:59 +0530

    fixing ip address issue

--- routes/movie.js
+++ routes/movie.js
@@ -1,26 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-const publicIp = require('public-ip');
-const {key, secret_key} = require('./.config.js');
+const axios = require("axios");
+const publicIp = require("public-ip");
+const { key, secret_key } = require("./.config.js");
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-    const forwarded = req.headers['x-forwarded-for'];
+    const forwarded = req.headers["x-forwarded-for"];
     const ip = forwarded
       ? forwarded.split(/, /)[0]
       : req.connection.remoteAddress;
-    const url = await get_movie(req.params.id, ip);
-    res.json({url: url});
+    const publicip = await publicIp.v6();
+
+    const url = await get_movie(req.params.id, publicip);
+    res.json({ url: url });
   } catch (err) {
     console.log(err);
     res.status(400);
   }
 });
 
-const get_movie = async (videoId, ip) => {
-  const ipaddress = ip; //await publicIp.v4();
-  console.log(`ipaddress : ${ipaddress}`);
+const get_movie = async (videoId, ipaddress) => {
+  console.log(`ipaddress  ${ipaddress}`);
 
   const ticketUrl = `https://videospider.in/getticket.php?key=${key}&secret_key=${secret_key}&video_id=${videoId}&ip=${ipaddress}`;
 
@@ -31,7 +32,7 @@ const get_movie = async (videoId, ip) => {
     console.log(`${url}`);
     return url;
   } catch (err) {
-    console.log('error', err);
+    console.log("error", err);
   }
 };
 
