7d1405756da6b465aad9c5338bc3117d4630fc3e
anilpdv <pdvanil007@gmail.com>
2022-10-13 01:17:19 +0530

    adding privacy policy

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,14 +1,18 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', function(req, res) {
-  res.render('index');
+router.get("/", function (req, res) {
+  res.render("index");
 });
 
-router.get('/home', function(req, res) {
-  res.render('home');
+router.get("/home", function (req, res) {
+  res.render("home");
 });
 
-router.get('/developer', function(req, res) {
-  res.render('developer');
+router.get("/developer", function (req, res) {
+  res.render("developer");
+});
+
+router.get("/privacy-policy", function (req, res) {
+  res.render("privacyPolicy");
 });
 module.exports = router;
